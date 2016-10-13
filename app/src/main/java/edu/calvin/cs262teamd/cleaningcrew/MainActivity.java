18b738d45942647ac88af7d57ddb31f878cb6d37
Caleb Postma <postmacaleb@gmail.com>
2016-10-13 13:16:27 -0400

    Implemented basic main and about pages
    Added a few checkable boxes to demonstrate the main page functionality

--- app/src/main/java/edu/calvin/cs262teamd/cleaningcrew/MainActivity.java
+++ app/src/main/java/edu/calvin/cs262teamd/cleaningcrew/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.calvin.cs262teamd.cleaningcrew;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("Housekeeping Log");
+    }
+
+    /**
+     * Determines behavior of the options menu in the action bar
+     * @param menu
+     * @return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles the user selection of an item in the options menu
+     * @param item
+     * @return true if selection valid, else call superclass method
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about_page:
+                startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
