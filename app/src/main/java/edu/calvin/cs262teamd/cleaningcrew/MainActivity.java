94ae2208be6db5b30635ed023cfb5a44c6cb72c4
Zach-Wibbenmeyer <zdw3@students.calvin.edu>
2016-12-13 17:08:19 -0500

    Updated Server

--- app/src/main/java/edu/calvin/cs262teamd/cleaningcrew/MainActivity.java
+++ app/src/main/java/edu/calvin/cs262teamd/cleaningcrew/MainActivity.java
@@ -1,19 +1,34 @@
 package edu.calvin.cs262teamd.cleaningcrew;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<MainTask> taskList = new ArrayList<>();
     private ListView taskListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +39,142 @@ public class MainActivity extends AppCompatActivity {
 
         taskListView = (ListView) findViewById(R.id.taskListView);
 
-        updateDisplay();
+        Log.d("begin", "get");
+        new GetPlayerTask().execute(createURL());
+        new GetTask().execute(createURL());
+
+        // updateDisplay();
+    }
+
+    /**
+     * Formats a URL for the webservice specified in the string resources.
+     *
+     * @return URL formatted for openweathermap.com
+     */
+    private URL createURL() {
+        try {
+            Log.d("URL", "create");
+            String urlString = "http://cs262.cs.calvin.edu:8084/cs262dCleaningCrew/task/cjp27";
+            return new URL(urlString);
+        } catch (Exception e) {
+//            Toast.makeText(this, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
+        }
+
+        return null;
+    }
+
+    /**
+     * Inner class for GETing the current weather data from openweathermap.org asynchronously
+     */
+    private class GetTask extends AsyncTask<URL, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(URL... params) {
+
+            Log.d("here", "test");
+            HttpURLConnection connection = null;
+            StringBuilder jsonText = new StringBuilder();
+            JSONArray new_result = null;
+            try {
+                JSONObject jsonData = new JSONObject();
+                //Log.d(new_url.toString(), "test");
+                connection = (HttpURLConnection) params[0].openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setFixedLengthStreamingMode(jsonData.toString().length());
+                DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                out.writeBytes(jsonData.toString());
+                out.flush();
+                out.close();
+                Log.d(params[0].toString(), "test");
+                Log.d("put output", connection.getInputStream().toString());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                Log.d("put output", "test");
+                connection.disconnect();
+            }
+
+            return null;
+        }
+    }
+    private class GetPlayerTask extends AsyncTask<URL, Void, JSONArray> {
+
+        @Override
+        protected JSONArray doInBackground(URL... params) {
+            Log.d("player", "test");
+            HttpURLConnection connection = null;
+            StringBuilder jsonText = new StringBuilder();
+            JSONArray result = null;
+            try {
+                connection = (HttpURLConnection) params[0].openConnection();
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+                    String line;
+                    Log.d("player", "here");
+                    while ((line = reader.readLine()) != null) {
+                        jsonText.append(line);
+                    }
+                    //Log.d(TAG, jsonText.toString());
+                    if (jsonText.charAt(0) == '[') {
+                        result = new JSONArray(jsonText.toString());
+                    } else if (jsonText.toString().equals("null")) {
+                        result = new JSONArray();
+                    } else {
+                        result = new JSONArray().put(new JSONObject(jsonText.toString()));
+                    }
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            Log.d("result", result.toString());
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray players) {
+            taskList.clear();
+            if (players == null) {
+                // Toast.makeText(MainActivity.this, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
+            } else if (players.length() == 0) {
+//                Toast.makeText(MainActivity.this, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
+            } else {
+                convertJSONtoArrayList(players);
+            }
+            MainActivity.this.updateDisplay();
+        }
+
+
+        /**
+         * Converts the JSON player data to an arraylist suitable for a listview adapter
+         *
+         * @param players JSON array of player objects
+         */
+        private void convertJSONtoArrayList(JSONArray players) {
+            try {
+                for (int i = 0; i < players.length(); i++) {
+                    JSONObject player = players.getJSONObject(i);
+                    taskList.add(new MainTask(
+                            player.getInt("id"),
+                            player.optString("description", "no name"),
+                            player.getInt("roomNumber"),
+                            player.optString("buildingName", "no email"),
+                            player.getBoolean("isComplete")
+                    ));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
@@ -44,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
      * @param item the selected menu item
      * @return true if selection valid, else call superclass method
      */
-    @Override 
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             // navigate to the about page
