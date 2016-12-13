3e3b171aa5133ee417928c50dd320dc04adc5a81
rgadams <roy.g.adams@gmail.com>
2016-12-13 16:53:22 -0500

    Added some functionality for database connection

--- app/src/main/java/edu/calvin/cs262teamd/cleaningcrew/EmployeeContactsActivity.java
+++ app/src/main/java/edu/calvin/cs262teamd/cleaningcrew/EmployeeContactsActivity.java
@@ -2,14 +2,17 @@ package edu.calvin.cs262teamd.cleaningcrew;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,14 +20,19 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class EmployeeContactsActivity extends AppCompatActivity {
 
     private ListView employeeListView;
     private JSONArray employees;
     private String search;
+
+    private List<Person> employeeList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +47,14 @@ public class EmployeeContactsActivity extends AppCompatActivity {
         // Set the search to its initial state
         search = "";
 
-        // Get the list of employees
-        getEmployees();
+//        // Get the list of employees
+//        getEmployees();
+
+        try {
+            new GetContactsTask().execute(new URL("http://cs262.cs.calvin.edu:8084/cs262dCleaningCrew/contact/cjp27"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         updateDisplay();
     }
@@ -90,27 +104,113 @@ public class EmployeeContactsActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    /*
-     * getEmployees()
-     *
-     * Currently reads in employees from a file, puts it in this class' employees JSONArray
-     *
-     * TODO: Change from reading from file to reading from DB.
+//    /*
+//     * getEmployees()
+//     *
+//     * Currently reads in employees from a file, puts it in this class' employees JSONArray
+//     *
+//     * TODO: Change from reading from file to reading from DB.
+//     */
+//    private void getEmployees() {
+//        // InputStream to the text file
+//        InputStream is = getBaseContext().getResources().openRawResource(R.raw.test_contacts);
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        StringBuilder sb = new StringBuilder();
+//        String line;
+//        try {
+//            while ((line = reader.readLine()) != null) {
+//                sb.append(line);
+//            }
+//            employees = new JSONObject(sb.toString()).getJSONArray("employees");
+//        } catch (java.io.IOException | org.json.JSONException ioe) {
+//            ioe.printStackTrace();
+//        }
+//    }
+
+    /**
+     * Inner class for GETing the player list from the course server asynchronously
      */
-    private void getEmployees() {
-        // InputStream to the text file
-        InputStream is = getBaseContext().getResources().openRawResource(R.raw.test_contacts);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
+    private class GetContactsTask extends AsyncTask<URL, Void, JSONArray> {
+
+        @Override
+        protected JSONArray doInBackground(URL... params) {
+            Log.d("test", "test");
+            HttpURLConnection connection = null;
+            StringBuilder jsonText = new StringBuilder();
+            JSONArray result = null;
+            try {
+                //Log.d("BLAH", "emp");
+                connection = (HttpURLConnection) params[0].openConnection();
+                //Log.d("BLAH", params[0].toString());
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    //Log.d("BLAH", "emp3");
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+                    String line;
+                    //Log.d("BLAH", "emp4");
+                    while ((line = reader.readLine()) != null) {
+                        jsonText.append(line);
+                    }
+                    //Log.d("BLAH", "emp5");
+                    result = new JSONArray(jsonText.toString());
+                    //Log.d("BLAH", "emp6");
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
             }
-            employees = new JSONObject(sb.toString()).getJSONArray("employees");
-        } catch (java.io.IOException | org.json.JSONException ioe) {
-            ioe.printStackTrace();
+            result = convertToEmployeeList(result);
+            Log.d("BLAH", result.toString());
+
+            return result;
         }
+
+        @Override
+        protected void onPostExecute(JSONArray employeeArray) {
+            employeeList.clear();
+            //Log.d("BLAH", "emp7");
+            if (employeeArray == null) {
+                Toast.makeText(EmployeeContactsActivity.this, "NULL Persons", Toast.LENGTH_SHORT).show();
+            } else if (employeeArray.length() == 0) {
+                Toast.makeText(EmployeeContactsActivity.this, "No Results", Toast.LENGTH_SHORT).show();
+            } else {
+                convertToEmployeeList(employeeArray);
+            }
+            //Log.d("BLAH", "emp8");
+            EmployeeContactsActivity.this.updateDisplay();
+        }
+
+    }
+
+    private JSONArray convertToEmployeeList(JSONArray employeeArray) {
+        try {
+            JSONArray emptest = new JSONArray();
+            Log.d("employeeArray length:", "" + employeeArray.length());
+            for (int i = 0; i < employeeArray.length(); i++) {
+                JSONObject emp = new JSONObject();
+                JSONObject empI = employeeArray.getJSONObject(i);
+                if(empI.has("emailaddress")) {
+                    emp.put("name", empI.getString("emailaddress"));
+                }
+                if(empI.has("name")) {
+                    emp.put("emailaddress", empI.getString("name"));
+                }
+                if(empI.has("phonenumber")) {
+                    emp.put("phonenumber", empI.getString("phonenumber"));
+                }
+                emptest.put(emp.toString());
+            }
+            Log.d("emptest", emptest.toString());
+            return emptest;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /*
@@ -124,7 +224,7 @@ public class EmployeeContactsActivity extends AppCompatActivity {
 
         int resource = R.layout.employee_list;
         String[] from = {"employee_name", "number", "email", "hours"};
-        int[] to = {R.id.employeeName, R.id.employeePhoneNumber, R.id.employeeEmailAddress, R.id.employeeHours};
+        int[] to = {R.id.employeeName, R.id.employeePhoneNumber, R.id.employeeEmailAddress};
 
         SimpleAdapter adapter = new SimpleAdapter(this, data, resource, from, to);
         employeeListView.setAdapter(adapter);
@@ -149,7 +249,6 @@ public class EmployeeContactsActivity extends AppCompatActivity {
                     map.put("employee_name", employee.getString("name"));
                     map.put("number", employee.getString("phone"));
                     map.put("email", employee.getString("email"));
-                    map.put("hours", employee.getString("hours"));
                     data.add(map);
                 }
             } catch (org.json.JSONException je) {
